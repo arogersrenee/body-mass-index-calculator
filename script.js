34a116d5b497fe27ee26be30b5463a03c02c4897
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-09-15 11:58:12 -0500

    update script for healthy weight range

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-/* ------ START ---------
+const numInputs = document.querySelectorAll("input[type=number]");
 
-When no input, welcome-msg is visible, hide results-summary. With input hide welcom-msg, show results-summary.
+const metricInputs = document.querySelectorAll(".metric-input");
+const imperialInputs = document.querySelectorAll(".imperial-input");
 
 
-.result-block (add/remove .welcome-block, .calc-bmi-results)
-.results-summary : (qSA * 2) : toggle hide
-#welcome-msg : toggle hide
-.hide
+const metricHeight = document.querySelector("#metric-height"); 
+const metricWeight = document.querySelector("#metric-weight");
 
------- END ---------*/
+const imperialHeightFt = document.querySelector("#imperial-height-ft"); 
+const imperialHeightIn = document.querySelector("#imperial-height-in");
+const imperialWeightSt = document.querySelector("#imperial-weight-st"); 
+const imperialWeightLbs = document.querySelector("#imperial-weight-lbs");
+
+const bmiScore = document.querySelector("#calc-bmi-results-score");
+const bmiClassification = document.querySelector("#calc-bmi-results-classification");
+
+const healthyLowerLimit = 18.5;
+const healthyUpperLimit = 24.9;
+const healthyBMI = document.querySelector("#calc-bmi-resutls-range");
+
+
+// When no input, welcome-msg is visible, hide results-summary. With input hide welcom-msg, show results-summary.
 
 (function toggleWelcome() {
     const bioInputs = document.querySelectorAll("input[type=number]");
@@ -49,23 +61,6 @@ function hideWelcome() {
         summary.classList.remove("hide")
     });
 }
-
-/* ------ START ---------
-
-When metric radio (#metric-unit) is selected: 
-    show calc-imperial-group, 
-    hide calc-imperial-weight-group. 
-When imperial radio (#imperial-unit) is selected: 
-    show calc-imperial-weight-group, 
-    hide calc-imperial-group.
-
-#metric-unit : radio btn
-#imperial-unit :  radio btn
-.calc-imperial-group : imperial view (qS *1)
-.calc-metric-group : metric view (qS *1)
-.hide
-
------- END ---------*/
 
 
 // radio buttons: select unit & reset values and summary
@@ -111,27 +106,11 @@ function resetSummary() {
     bmiRange.textContent = "..";
 }
 
-const numInputs = document.querySelectorAll("input[type=number]");
 
-const metricInputs = document.querySelectorAll(".metric-input");
-const imperialInputs = document.querySelectorAll(".imperial-input");
-
-
-const metricHeight = document.querySelector("#metric-height"); 
-const metricWeight = document.querySelector("#metric-weight");
-
-const imperialHeightFt = document.querySelector("#imperial-height-ft"); 
-const imperialHeightIn = document.querySelector("#imperial-height-in");
-const imperialWeightSt = document.querySelector("#imperial-weight-st"); 
-const imperialWeightLbs = document.querySelector("#imperial-weight-lbs");
-
-const bmiScore = document.querySelector("#calc-bmi-results-score");
-const bmiClassification = document.querySelector("#calc-bmi-results-classification");
-
-
+//calculator functions
 metricInputs.forEach((input) => {
     input.addEventListener("keyup", () => {
-        if (metricHeight.value.trim() > 0 && metricWeight.value.trim() > 0){
+        if (metricHeight.value.trim() > 0){
             bmiScore.textContent = calculateBMI(...convertMetricUnits());
             updateClassification();
             updateMetricResultsRange();
@@ -148,19 +127,6 @@ imperialInputs.forEach((input) => {
         }
     })
 })
-// numInputs.forEach((input) => {
-//     input.addEventListener("keyup", () => {
-//         if (metricHeight.value.trim() > 0 && metricWeight.value.trim() > 0){
-//             bmiScore.textContent = calculateBMI(...convertMetricUnits());
-//             updateClassification();
-//             updateMetricResultsRange();
-//         }  else if (imperialHeightFt.value.trim() > 0 && imperialWeightSt.value.trim() > 0) {
-//             bmiScore.textContent = calculateBMI(...convertImperialUnits());
-//             updateClassification();
-//             updateImperialResultsRange();
-//         }
-//     })
-// })
 
 
 
@@ -222,232 +188,30 @@ function updateClassification() {
 }
 
 function updateMetricResultsRange() {
-    const metricHeight = document.querySelector("#metric-height");
-    let centimeters = metricHeight.value.trim();
-    height = Math.ceil((Number(centimeters) / 2.54));
-    const healthyBMI = document.querySelector("#calc-bmi-resutls-range");
+    let height = Number(metricHeight.value.trim());
 
-    switch (height) {
-        case (58): healthyBMI.textContent = "41.2kgs - 52.2kgs";
-            break;
-        case (59): healthyBMI.textContent = "42.6kgs - 54kgs";
-            break;
-        case (60): healthyBMI.textContent = "44.0kgs - 55.8kgs";
-            break;
-        case (61): healthyBMI.textContent = "45.5kgs - 57.6kgs";
-            break;
-        case (62): healthyBMI.textContent = "47.2kgs - 59.4kgs";
-            break;
-        case (63): healthyBMI.textContent = "48.5kgs - 61.2kgs";
-            break; 
-        case (64): healthyBMI.textContent = "49.9kgs - 63.5kgs";
-            break;
-        case (65): healthyBMI.textContent = "51.7kgs - 65.3kgs";
-            break;
-        case (66): healthyBMI.textContent = "53.5kgs - 67.1kgs";
-            break;
-        case (67): healthyBMI.textContent = "54.9kgs - 69.4kgs";
-            break;
-        case (68): healthyBMI.textContent = "56.7kgs - 71.7kgs";
-            break;
-        case (69): healthyBMI.textContent = "58.1kgs - 73.5kgs";
-            break;
-        case (70): healthyBMI.textContent = "59.9kgs - 75.5kgs";
-            break;
-        case (71): healthyBMI.textContent = "61.7kgs - 78.0kgs";
-            break;
-        case (72): healthyBMI.textContent = "63.5kgs - 80.3kgs";
-            break;
-        case (73): healthyBMI.textContent = "65.3kgs - 82.5kgs";
-            break;
-        case (74): healthyBMI.textContent = "67.3kgs - 84.6kgs";
-            break;
-        case (75): healthyBMI.textContent = "68.9kgs - 87.0kgs";
-            break;
-        case (76): healthyBMI.textContent = "70.8kgs - 89.5kgs";
-            break;
-    }
+    const upperWeight = Math.round(((healthyUpperLimit / 10000) * (height**2)) * 10) / 10;
+    const lowerWeight = Math.round(((healthyLowerLimit / 10000) * (height**2)) * 10) / 10;
+
+    healthyBMI.textContent = `${lowerWeight}kgs - ${upperWeight}kgs`;
 }
+
 
 function updateImperialResultsRange() {
-    const imperialHeightFt = document.querySelector("#imperial-height-ft"); 
-    const imperialHeightIn = document.querySelector("#imperial-height-in");
-    let feet = imperialHeightFt.value.trim();
-    let inches = imperialHeightIn.value.trim();
-    height = Math.ceil((Number(feet) * 12) + Number(inches));
-    const healthyBMI = document.querySelector("#calc-bmi-resutls-range");
+    let feet = Number(imperialHeightFt.value.trim());
+    let inches = Number(imperialHeightIn.value.trim());
+    height = (feet * 12) + inches;
 
-    switch (height) {
-        case (58): healthyBMI.textContent = "6st 7lbs - 8st 3lbs";
-            break;
-        case (59): healthyBMI.textContent = "6st 10lbs - 8st 7lbs";
-            break;
-        case (60): healthyBMI.textContent = "6st 13lbs - 8st 11lbs";
-            break;
-        case (61): healthyBMI.textContent = "7st 2lbs - 9st 1lbs";
-            break;
-        case (62): healthyBMI.textContent = "7st 6lbs - 9st 5lbs";
-            break;
-        case (63): healthyBMI.textContent = "7st 9lbs - 9st 9lbs";
-            break;
-        case (64): healthyBMI.textContent = "7st 12lbs - 10st";
-            break;
-        case (65): healthyBMI.textContent = "8st 2lbs - 10st 4lbs";
-            break;
-        case (66): healthyBMI.textContent = "8st 6lbs - 10st 8lbs";
-            break;
-        case (67): healthyBMI.textContent = "8st 9lbs - 10st 13lbs";
-            break;
-        case (68): healthyBMI.textContent = "8st 13lbs - 11st 4lbs";
-            break;
-        case (69): healthyBMI.textContent = "9st 2lbs - 11st 8lbs";
-            break;
-        case (70): healthyBMI.textContent = "9st 6lbs - 11st 13lbs";
-            break;
-        case (71): healthyBMI.textContent = "9st 10lbs - 12st 4lbs";
-            break;
-        case (72): healthyBMI.textContent = "10st - 12st 9lbs";
-            break;
-        case (73): healthyBMI.textContent = "10st 4lbs - 13st";
-            break;
-        case (74): healthyBMI.textContent = "10st 8lbs - 13st 4lbs";
-            break;
-        case (75): healthyBMI.textContent = "10st 12lbs - 13 10lbs";
-            break;
-        case (76): healthyBMI.textContent = "11st 2lbs - 14st 1lb";
-            break;
-    }
+    const lowerWeight = Math.round(((healthyLowerLimit / 703) * (height**2)) * 10) / 10;
+    const upperWeight = Math.round(((healthyUpperLimit / 703) * (height**2)) * 10) / 10;
+    const lowerWeightSt = Math.floor(lowerWeight/14);
+    const lowerWeightLbs = Math.floor(lowerWeight%14);
+    const upperWeightSt = Math.floor(upperWeight/14);
+    const upperWeightLbs = Math.floor(upperWeight%14);
 
+    healthyBMI.textContent = `${lowerWeightSt}st ${lowerWeightLbs}lbs - ${upperWeightSt}st ${upperWeightLbs}lbs`
 }
 
-
-
-/* ------ START ---------
-
-When metric radio is selected: 
-
-The metric formula for BMI: 
-BMI = weight (kg) / (height(meters)*2) : is weight in kilograms divided by height in meters squared.
-
-#metric-height : input (value: convert centimeters to meters)
-#metric-weight : input (value)
-
-#calc-bmi-results-score : placeholder (equals BMI calculation)
-return BMI
-
-
-
-OR -->> use IMPERIAL formula
-BMI = (weight (lbs) / (height(inches)*2)) * 703
-
-#metric-height : input (value: convert centimeters to inches)
-#metric-weight : input (value: convert kg to lbs)
-
------- END ---------*/
-
-
-
-/* ------ START ---------
-
-When imperial radio is selected: 
-
-The imperial formula for BMI: 
-BMI = (weight (lbs) / (height(inches)*2)) * 703 : is your weight in pounds (lbs) divided by your height in inches, squared, and then you multiply this figure by a conversion factor of 703.
-
-#imperial-height-ft : input (value: convert to inches)
-#imperial-height-in : input (add to height-ft - ft(converted first))
-
-#imperial-weight-st : input (value: convert to lbs)
-#imperial-weight-lbs : input (add to weight-st(converted first))
-
-#calc-bmi-results-score : placeholder (equals BMI calculation)
-return BMI
-
------- END ---------*/
-
-
-
-
-/* ------ START ---------
-Classifications: 
-
-#calc-bmi-results-classification : placeholder
-    if BMI less than 18.5: you're underweight
-    if BMI 18.5 to 24.9 : you're a healthy weight
-    if BMI 25 to 29.9 : you're overweight
-    if BMI 30 or greater : you're obese
-
------- END ---------*/
-
-
-
-
-
-
-/* ------ START ---------
-
-Healthy weight range: (metric)
-
-#calc-bmi-resutls-range : placeholder
-    if Height:
-(height 58 in)	41.2kgs - 52.2kgs
-(height 59 in)	42.6kgs - 54kgs |
-(height 60 in)  44.0kgs - 55.8kgs
-(height 61 in)	45.5kgs - 57.6kgs
-(height 62 in)	47.2kgs - 59.4kgs
-(height 63 in)	48.5kgs - 61.2kgs 
-(height 64 in)	49.9kgs - 63.5kgs
-(height 65 in)	51.7kgs - 65.3kgs
-(height 66 in)	53.5kgs - 67.1kgs
-(height 67 in)	54.9kgs - 69.4kgs
-(height 68 in)	56.7kgs - 71.7kgs
-(height 69 in)	58.1kgs - 73.5kgs
-(height 70 in)	59.9kgs - 75.5kgs
-(height 71 in)	61.7kgs - 78.0kgs
-(height 72 in)	63.5kgs - 80.3kgs
-(height 73 in)	65.3kgs - 82.5kgs
-(height 74 in)	67.3kgs - 84.6kgs
-(height 75 in)	68.9kgs - 87.0kgs
-(height 76 in)	70.8kgs - 89.5kgs
-
------- END ---------*/
-
-
-
-
-/* ------ START ---------
-
-Healthy weight range: (imperial)
-
-#calc-bmi-resutls-range : placeholder
-    if Height:
-(height 58 in)	6st 7lbs - 8st 3lbs
-(height 59 in)	6st 10lbs - 8st 7lbs
-(height 60 in)  6st 13lbs - 8st 11lbs
-(height 61 in)	7st 2lbs - 9st 1lbs
-(height 62 in)	7st 6lbs - 9st 5lbs
-(height 63 in)	7st 9lbs - 9st 9lbs	
-(height 64 in)	7st 12lbs - 10st
-(height 65 in)	8st 2lbs - 10st 4lbs
-(height 66 in)	8st 6lbs - 10st 8lbs
-(height 67 in)	8st 9lbs - 10st 13lbs
-(height 68 in)	8st 13lbs - 11st 4lbs
-(height 69 in)	9st 2lbs - 11st 8lbs
-(height 70 in)	9st 6lbs - 11st 13lbs
-(height 71 in)	9st 10lbs - 12st 4lbs
-(height 72 in)	10st - 12st 9lbs
-(height 73 in)	10st 4lbs - 13st
-(height 74 in)	10st 8lbs - 13st 4lbs
-(height 75 in)	10st 12lbs - 13 10lbs
-(height 76 in)	11st 2lbs - 14st 1lb
-
------- END ---------*/
-
-
-
-
-
-// .hide
 
 
 
@@ -455,7 +219,8 @@ Healthy weight range: (imperial)
 //https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
 
 
-
+// round one decimal place 
+// https://stackoverflow.com/questions/7342957/how-do-you-round-to-one-decimal-place-in-javascript
 
 
 
